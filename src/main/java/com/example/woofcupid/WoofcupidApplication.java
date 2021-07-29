0360a726aa3bbcdd2f61e207ca4c969b22853eb8
Krzysiek <kzadka89@gmail.com>
2021-07-29 20:34:15 +0200

    Initial commit

---
+++ src/main/java/com/example/woofcupid/WoofcupidApplication.java
@@ -0,0 +1,13 @@
+package com.example.woofcupid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoofcupidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoofcupidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/woofcupid/WoofcupidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woofcupid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoofcupidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
